d26f726f6387fb5a590a19f4679bdc39ca449e30
Ambre-Maurugeon <129891903+Ambre-Maurugeon@users.noreply.github.com>
2024-10-15 18:50:51 +0200

    [EDIT]Drag

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -3,21 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour, IPointerDownHandler
+public class Slot : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject objectPrefab; 
+
+    //Hover Scale
+    private Vector3 _scaleInit;
+    [SerializeField] private GameObject _slotSprite;
+    
     private GameObject currentDraggedObject;
 
+    void Start(){
+      _scaleInit = _slotSprite.transform.localScale;
+      _slotSprite.transform.localScale = _scaleInit;
+    }
+
+//Drag
   public void OnPointerDown(PointerEventData eventData)
-{
-    GameObject instantiatedObject = Instantiate(objectPrefab, transform.position, Quaternion.identity);
+  {
+      GameObject instantiatedObject = Instantiate(objectPrefab, transform.localPosition, Quaternion.identity);
 
-    instantiatedObject.transform.SetParent(transform.parent);
+      instantiatedObject.transform.SetParent(objectPrefab.transform.parent);
 
-    instantiatedObject.transform.localPosition = Vector3.zero;
+      DragHandler.Instance.StartDragging(instantiatedObject);
+  }
 
-    DragHandler.Instance.StartDragging(instantiatedObject);
-}
+//Hover
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+       _slotSprite.transform.localScale = _scaleInit *1.1f;
+       Debug.Log("yaaaa");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _slotSprite.transform.localScale = _scaleInit;
+    }
 
 
 
